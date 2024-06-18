724bdfb24749c0aae71248dc10ad4a2699f9d1c7
Kein010 <151629605+Kein010@users.noreply.github.com>
2024-06-18 09:10:40 +0200

    VS Auto changes

--- LF08Nr2/LF08Nr2/obj/Debug/net8.0-windows/LF08Nr2.AssemblyInfo.cs
+++ LF08Nr2/LF08Nr2/obj/Debug/net8.0-windows/LF08Nr2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LF08Nr2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f29acc1eb6c3fd232f6256e6b3b807cca3c345ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("LF08Nr2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LF08Nr2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
